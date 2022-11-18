5a709114fb46d89c4abad90945700f449f2b0807
Joel Maciel <soldado25461@gmail.com>
2022-11-18 11:33:55 -0300

    feat: initial commit of calculator

--- src/main/java/br/com/joel/MathController.java
+++ src/main/java/br/com/joel/MathController.java
@@ -3,21 +3,25 @@ package br.com.joel;
 import java.util.concurrent.atomic.AtomicLong;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping
-public class GreetingController {
+public class MathController {
 	
-	private static final String template = "Hello, %s!";
+	
 	private  final AtomicLong counter = new AtomicLong();
 	
-	@GetMapping("/greeting")
-	public Greeting greeting (@RequestParam(value = "name", defaultValue = "World") String name) {
-		return new Greeting(counter.incrementAndGet(), String.format(template, name));
-
+	@GetMapping(value = "/sum/{numberOne}/{numberTwo}")
+	public Double sum(@PathVariable(value = "numberOne") String numberOne,
+					  @PathVariable(value = "numberTwo") String numberYwo) {
+		return 1D;
 	}
 
 }
+
+
+
+
