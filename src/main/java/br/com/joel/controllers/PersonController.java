30f7873f66bf9436861c1685043595283f186db2
Joel Maciel <soldado25461@gmail.com>
2022-11-18 21:50:14 -0300

    feat: implemented partial crud

--- src/main/java/br/com/joel/controllers/PersonController.java
+++ src/main/java/br/com/joel/controllers/PersonController.java
@@ -3,8 +3,12 @@ package br.com.joel.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,9 +28,31 @@ public class PersonController {
 	}
 	
 	@GetMapping(value = "/{personId}")
-	public Person findById(@PathVariable(value = "personId") String personId) {
+	public Person findById(@PathVariable(value = "personId") Long personId) {
 
 		return personServices.findById(personId);
 	}
+	
+	@PostMapping
+	public Person create(@RequestBody Person person) {
+		personServices.create(person);
+		return person;
+	}
+	
+	@PutMapping("/{personId}")
+	public Person update(@RequestBody Person person) {
+		personServices.update(person);
+		return person;
+	}
+	
+	@DeleteMapping("/{personId}")
+	public void delete(@PathVariable("personId") Long personId) {
+		personServices.delete(personId);
+	}
 
 }
+
+
+
+
+
