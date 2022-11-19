41b5ddadd790cb9d3d0d89e57ec850963b9f0e3b
Joel Maciel <soldado25461@gmail.com>
2022-11-18 21:06:05 -0300

    feat: endpoint findById

--- src/main/java/br/com/joel/controllers/PersonController.java
+++ src/main/java/br/com/joel/controllers/PersonController.java
@@ -1,5 +1,7 @@
 package br.com.joel.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,7 +17,12 @@ public class PersonController {
 
 	@Autowired
 	private PersonServices personServices;
-
+	
+	@GetMapping
+	public List<Person> findAll() {
+		return  personServices.findAll();
+	}
+	
 	@GetMapping(value = "/{personId}")
 	public Person findById(@PathVariable(value = "personId") String personId) {
 
--- src/main/java/br/com/joel/services/PersonServices.java
+++ src/main/java/br/com/joel/services/PersonServices.java
@@ -1,5 +1,7 @@
 package br.com.joel.services;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.logging.Logger;
 
@@ -13,6 +15,16 @@ public class PersonServices {
 	private final AtomicLong counter = new AtomicLong();
 	private Logger logger = Logger.getLogger(PersonServices.class.getName());
 	
+	public List<Person> findAll() {
+		List<Person> persons = new ArrayList<>();
+		
+		for (int i = 0; i < 8; i++) {
+			Person person = mockPerson(i);
+			persons.add(person);
+		}
+		return persons;
+	}
+
 	public Person findById(String personId) {
 		
 		logger.info("Finding one person !");
@@ -24,6 +36,19 @@ public class PersonServices {
 		person.setAddress("Caucauia - Ceará");
 		person.setGender("Male");
 		
+		return person;
+	}
+	
+	private Person mockPerson(int i) {
+		
+		logger.info("Finding all people !");
+		
+		var person = new Person();
+		person.setId(counter.incrementAndGet());
+		person.setFirstName("Person name " + i);
+		person.setLastName("Last name " + i);
+		person.setAddress("Some address in Brasil " + i);
+		person.setGender("Male");
 		return person;
 	}
 
