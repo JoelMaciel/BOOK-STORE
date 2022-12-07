f675d50394f3f1fb8c496ae11cd7bd03a5daae86
Joel Maciel <soldado25461@gmail.com>
2022-12-07 14:38:09 -0300

    feat: Adding migratios to the database to support Authentication

---
+++ src/main/resources/db/migration/V5__Create_Table_Permission.sql
@@ -0,0 +1,5 @@
+﻿CREATE TABLE IF NOT EXISTS `permission` (
+  `permission_id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `description` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`permission_id`)
+) ENGINE=InnoDB;
---
+++ src/main/resources/db/migration/V9__Create_Table_User_Permission.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE IF NOT EXISTS `user_permission` (
+  `user_id` bigint(20) NOT NULL,
+  `permission_id` bigint(20) NOT NULL,
+  PRIMARY KEY (`user_id`,`permission_id`),
+  KEY `fk_user_permission_permission` (`permission_id`),
+  CONSTRAINT `fk_user_permission` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`),
+  CONSTRAINT `fk_user_permission_permission` FOREIGN KEY (`permission_id`) REFERENCES `permission` (`permission_id`)
+) ENGINE=InnoDB;
